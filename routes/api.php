0e33ecf4a400a84bcef07955f40d7d7ed78ebd5f
Clement Maldonado <clement.maldo@gmail.com>
2023-04-13 13:08:12 +0000

    Included controllers'

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\MessageController;
 
 /*
 |--------------------------------------------------------------------------
