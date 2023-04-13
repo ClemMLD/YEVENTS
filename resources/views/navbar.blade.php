4d85c7a32b4a559d3a4f28b1fc365fd7e956bd98
Clement Maldonado <clement.maldo@gmail.com>
2023-04-13 15:15:25 +0000

    Added navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,51 +1,55 @@
+<link rel="stylesheet" href="{{ asset('navbar.css') }}">
 <nav class="navbar navbar-expand-lg navbar-light">
-          <a class="navbar-brand" href="#">Yevents</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="#">Yevents</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="#">Acceuil<span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Campus
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Aix en provence</a>
-            <a class="dropdown-item" href="#">Bordeaux</a>
-            <a class="dropdown-item" href="#">Lille</a>
-            <a class="dropdown-item" href="#">Lyon</a>
-            <a class="dropdown-item" href="#">Montpellier</a>
-            <a class="dropdown-item" href="#">Nantes</a>
-            <a class="dropdown-item" href="#">Paris</a>
-            <a class="dropdown-item" href="#">Rennes</a>
-            <a class="dropdown-item" href="#">Sophia</a>
-            <a class="dropdown-item" href="#">Toulouse</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Tous les campus</a>
-            </div>
-        </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Types d'évenements
-        </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Cinema</a>
-                <a class="dropdown-item" href="#">Restaurant</a>
-                <a class="dropdown-item" href="#">Sport</a>
-                <a class="dropdown-item" href="#">Soiree</a>
-                <a class="dropdown-item" href="#">Concert</a>
-                <a class="dropdown-item" href="#">Culture</a>
-                <a class="dropdown-item" href="#">Jeux vidéo</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Autre</a>
-            </div>
-    </li>
-    
-</ul>
-<img src="{{ asset('images/logo.svg') }}" alt="logo" class="logo">
-</div>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Acceuil<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Campus
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Aix en provence</a>
+                    <a class="dropdown-item" href="#">Bordeaux</a>
+                    <a class="dropdown-item" href="#">Lille</a>
+                    <a class="dropdown-item" href="#">Lyon</a>
+                    <a class="dropdown-item" href="#">Montpellier</a>
+                    <a class="dropdown-item" href="#">Nantes</a>
+                    <a class="dropdown-item" href="#">Paris</a>
+                    <a class="dropdown-item" href="#">Rennes</a>
+                    <a class="dropdown-item" href="#">Sophia</a>
+                    <a class="dropdown-item" href="#">Toulouse</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Tous les campus</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Types d'évenements
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Cinema</a>
+                    <a class="dropdown-item" href="#">Restaurant</a>
+                    <a class="dropdown-item" href="#">Sport</a>
+                    <a class="dropdown-item" href="#">Soiree</a>
+                    <a class="dropdown-item" href="#">Concert</a>
+                    <a class="dropdown-item" href="#">Culture</a>
+                    <a class="dropdown-item" href="#">Jeux vidéo</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Autre</a>
+                </div>
+            </li>
+
+        </ul>
+        <img src="{{ asset('images/logo.svg') }}" alt="logo" class="logo">
+    </div>
 </nav>
