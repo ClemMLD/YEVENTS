8d1b2807d4d098a704e6bfa750526a524c9d008e
Clement Maldonado <clement.maldo@gmail.com>
2023-04-13 10:49:45 +0000

    Added likes and attendees

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Event;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class EventsController extends Controller
 {
@@ -25,6 +27,33 @@ class EventsController extends Controller
     public function mostLikedEvents() // Add likes in migration first
     {
         return Event::orderBy('likes', 'desc')->get();
+    }
+
+    public function eventAttendees($id)
+    {
+        $event = Event::find($id);
+
+        if (!$event) {
+            return response()->json(['error' => 'Event not found'], 404);
+        }
+
+        return array_map(function ($id) {
+            return User::find($id)->nickname;
+        }, json_decode($event->attendees));
+    }
+
+    public function subscribeToEvent($id)
+    {
+        if (!auth()->user()) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        $event = Event::where('id', $id)->first();
+        Event::where('id', $id)->update([
+            'attendees' => json_encode(array_merge(json_decode($event->attendees), [auth()->user()->id])),
+        ]);
+
+        return response()->json(['success' => 'You are now subscribed to this event'], 200);
     }
 
     public function createEvent(Request $request)
